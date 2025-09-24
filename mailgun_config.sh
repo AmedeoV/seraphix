74bdc90cfefcd9be1f2d66b7d2559c5de3847ea4
vertulloa <20638889+AmedeoV@users.noreply.github.com>
2025-09-24 09:59:57 +0100

    Push empty mailgun config

---
+++ mailgun_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Mailgun API Configuration
+export MAILGUN_API_KEY="MAILGUN_API_KEY" # Your Mailgun API key
+export MAILGUN_DOMAIN="MAILGUN_DOMAIN"  # Your Mailgun domain
+export MAILGUN_FROM="MAILGUN_FROM"     # Your Mailgun from email address
+
+# Default notification settings
+export DEFAULT_EMAIL="DEFAULT_EMAIL"   # Default email address to send notifications to
